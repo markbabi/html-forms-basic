30a7fd5029a72b1be344b6e5c459d2db586629b8
Mark Babirad <mbabirad@brainstation.io>
2024-02-06 14:19:54 -0800

    initialize project

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+// our one Source of Truth
+const orders = [
+  {
+    name: "Poppa Pizza Eater",
+    email: "pops@pizza.com",
+    order: "1 large pepperoni",
+    date: Date.now(),
+  },
+  {
+    name: "Mom Mozza Muncher",
+    email: "moms@mozza.com",
+    order: "Big ol' ball of mozzarella!",
+    date: Date.now(),
+  },
+  {
+    name: "Sunny Sausage",
+    email: "sunny@sausage.ca",
+    order: "Some cheesesticks and a sprite",
+    date: Date.now(),
+  },
+];
